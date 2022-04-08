a3be34e4173a2a49241998f8d6e7486930fce7e9
Emily Love (724142) <ELove@smail.pembinatrails.ca>
2022-04-08 13:59:09 -0500

    maybe working

--- memory/scripts/mem.js
+++ memory/scripts/mem.js
@@ -203,3 +203,47 @@ for (var i = 0; i < cards.length; i++) {
   card.addEventListener("click", cardOpen);
   card.addEventListener("click", congratulations);
 }
+
+// scorebord
+
+function checkHighScore(score) {
+  const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
+  const lowestScore = highScores[NO_OF_HIGH_SCORES - 1]?.score ?? 0;
+  
+  if (score > lowestScore) {
+    saveHighScore(score, highScores); 
+    showHighScores(); 
+  }
+}
+
+function saveHighScore(score, highScores) {
+  const name = prompt('You got a highscore! Enter name:');
+  const newScore = { score, name };
+  
+  // 1. Add to list
+  highScores.push(newScore);
+
+  // 2. Sort the list
+  highScores.sort((a, b) => b.score - a.score);
+  
+  // 3. Select new list
+  highScores.splice(NO_OF_HIGH_SCORES);
+  
+  // 4. Save to local storage
+  localStorage.setItem(HIGH_SCORES, JSON.stringify(highScores));
+};
+
+const highScoreList = document.getElementById(HIGH_SCORES);
+
+highScoreList.innerHTML = highScores.map((score) => 
+  `<li>${score.score} - ${score.name}`
+);
+
+function showHighScores() {
+    const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
+    const highScoreList = document.getElementById(HIGH_SCORES);
+    
+    highScoreList.innerHTML = highScores
+      .map((score) => `<li>${score.score} - ${score.name}`)
+      .join('');
+}
