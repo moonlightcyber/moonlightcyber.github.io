64ae84dd5dae12749cde3c2d7cf9d283501685ba
Emily Love (724142) <ELove@smail.pembinatrails.ca>
2022-04-19 13:52:36 -0500

    fixing things

--- gps/scripts/gps.js
+++ gps/scripts/gps.js
@@ -22,3 +22,14 @@ function getLocation(postiton) {
         });
     }
 }
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+ 
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
